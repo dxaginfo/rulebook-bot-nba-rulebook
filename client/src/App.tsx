dc27a4f9f429fcb40db5100c72606d0d9748add1
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-06-17 19:52:02 -0300

    Add App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,65 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider, createTheme, CssBaseline, Box, Container } from '@mui/material';
-import { ChatProvider } from './contexts/ChatContext';
-import { RuleProvider } from './contexts/RuleContext';
+import { ThemeProvider, CssBaseline, Container, Box } from '@mui/material';
+import theme from './theme';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
-import RulePage from './pages/RulePage';
 import AboutPage from './pages/AboutPage';
+import RulePage from './pages/RulePage';
 import NotFoundPage from './pages/NotFoundPage';
+import { ChatProvider } from './contexts/ChatContext';
+import { RuleProvider } from './contexts/RuleContext';
 
-// Create a custom theme
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#006BB6', // NBA blue
-      dark: '#00487A',
-      light: '#338DC7'
-    },
-    secondary: {
-      main: '#ED174C', // NBA red
-      dark: '#BF0D35',
-      light: '#F14971'
-    },
-    background: {
-      default: '#FAFAFA'
-    }
-  },
-  typography: {
-    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
-    h1: {
-      fontWeight: 700
-    },
-    h2: {
-      fontWeight: 600
-    },
-    h3: {
-      fontWeight: 600
-    }
-  },
-  components: {
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          borderRadius: 8,
-          textTransform: 'none',
-          fontWeight: 600
-        }
-      }
-    },
-    MuiPaper: {
-      styleOverrides: {
-        root: {
-          borderRadius: 8
-        }
-      }
-    }
-  }
-});
-
-const App: React.FC = () => {
+function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
@@ -73,23 +25,19 @@ const App: React.FC = () => {
             }}>
               <Header />
               
-              <Container 
-                component="main" 
-                maxWidth="lg" 
-                sx={{ 
-                  flexGrow: 1, 
-                  py: 4,
-                  display: 'flex',
-                  flexDirection: 'column'
-                }}
-              >
-                <Routes>
-                  <Route path="/" element={<HomePage />} />
-                  <Route path="/rule/:id" element={<RulePage />} />
-                  <Route path="/about" element={<AboutPage />} />
-                  <Route path="*" element={<NotFoundPage />} />
-                </Routes>
-              </Container>
+              <Box sx={{ 
+                flexGrow: 1, 
+                py: 4 
+              }}>
+                <Container maxWidth="lg">
+                  <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/about" element={<AboutPage />} />
+                    <Route path="/rule/:id" element={<RulePage />} />
+                    <Route path="*" element={<NotFoundPage />} />
+                  </Routes>
+                </Container>
+              </Box>
               
               <Footer />
             </Box>
@@ -98,6 +46,6 @@ const App: React.FC = () => {
       </ChatProvider>
     </ThemeProvider>
   );
-};
+}
 
 export default App;
