452cf1de6905f5dd0c779547bbb36a3547164d04
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-06-17 20:11:21 -0300

    Add App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,40 +1,44 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider, CssBaseline, Container } from '@mui/material';
+import { ThemeProvider, CssBaseline, Box } from '@mui/material';
+
 import theme from './theme';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import RulePage from './pages/RulePage';
-import NotFoundPage from './pages/NotFoundPage';
 import { ChatProvider } from './contexts/ChatContext';
 import { RuleProvider } from './contexts/RuleContext';
 
-const App: React.FC = () => {
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import RulePage from './pages/RulePage';
+import AboutPage from './pages/AboutPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <ChatProvider>
-        <RuleProvider>
+      <RuleProvider>
+        <ChatProvider>
           <Router>
-            <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
               <Header />
-              <Container component="main" sx={{ flexGrow: 1, py: 4 }}>
+              
+              <Box component="main" sx={{ flexGrow: 1, py: 4 }}>
                 <Routes>
                   <Route path="/" element={<HomePage />} />
-                  <Route path="/about" element={<AboutPage />} />
                   <Route path="/rule/:id" element={<RulePage />} />
+                  <Route path="/about" element={<AboutPage />} />
                   <Route path="*" element={<NotFoundPage />} />
                 </Routes>
-              </Container>
+              </Box>
+              
               <Footer />
-            </div>
+            </Box>
           </Router>
-        </RuleProvider>
-      </ChatProvider>
+        </ChatProvider>
+      </RuleProvider>
     </ThemeProvider>
   );
-};
+}
 
 export default App;
